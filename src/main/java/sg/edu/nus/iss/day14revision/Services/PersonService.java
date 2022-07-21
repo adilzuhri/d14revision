c2732bedc660eacad64b5b857152a57520e8e07a
Adil Zuhri <106576506+adilzuhri@users.noreply.github.com>
2022-07-21 10:55:49 +0800

    added html and css

---
+++ src/main/java/sg/edu/nus/iss/day14revision/Services/PersonService.java
@@ -0,0 +1,29 @@
+package sg.edu.nus.iss.day14revision.Services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import sg.edu.nus.iss.day14revision.Models.Person;
+
+@Service
+public class PersonService {
+    private List<Person> persons = new ArrayList<Person>();
+
+    public PersonService() {
+        persons.add(new Person("Jeff", "Bezo"));
+        persons.add(new Person("Tim", "Cook"));
+                
+    }
+
+    public List<Person> gPersons(){
+        return this.persons;
+    }
+
+    public void addPerson(Person p) {
+        persons.add(new Person(p.getFirstName(), p.getLastName()));
+    }
+
+    
+}
