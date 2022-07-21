918cd641f58e39710c5a5957d07f3d1e2a007024
Adil Zuhri <106576506+adilzuhri@users.noreply.github.com>
2022-07-21 09:48:44 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/day14revision/Day14revisionApplication.java
@@ -0,0 +1,41 @@
+package sg.edu.nus.iss.day14revision;
+
+import java.util.Collections;
+
+import org.springframework.boot.DefaultApplicationArguments;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@SpringBootApplication
+public class Day14revisionApplication {
+
+	public static void main(String[] args) 
+		//SpringApplication.run(Day14revisionApplication.class, args);
+
+
+		SpringApplication app = new SpringApplication(...primarySources:Day14revisionApplication.class
+	
+		String port=8085;
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+
+		if (cliOpts.containsOption("port")) {
+			port = cliOpts.getOptionValues("port").get(0);
+
+		app.setDefaultProperties(
+			Collections.singletonMap("server.port", port));
+
+			app.run(args);
+		}
+
+
+	@Bean
+	public CommonsRequestLoggingFilter log() {
+		CommonsRequestLoggingFilter logger = new CommonsRequestLoggingFilter();
+		logger.setIncludeClientInfo(true);
+		logger.setIncludeQueryString(true);	
+		return logger;
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/day14revision/Day14revisionApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day14revision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day14revisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
